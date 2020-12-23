e90cfcee35ba43b5380378e002773573f2f62cb9
Borguuh <abdrahmanadewale@gmail.com>
2020-12-23 22:24:19 +0100

    setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const engine = require('ejs-mate');
+const session = require('express-session');
+
+
+const app = express();
+
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.listen(3000, () =>{
+	console.log('running on port 3000');
+})
